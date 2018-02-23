1ec1893ccd91239fe8a8a3cec469d7c7e51c1c2c
Remi Kristelijn <rkristelijn@gmail.com>
2018-02-23 11:37:42 +0000

    added more tests, but not done

--- api/books/book.integration.test.js
+++ api/books/book.integration.test.js
@@ -2,7 +2,15 @@ const expect = require('chai').expect;
 const app = require('../../');
 const request = require('supertest')(app);
 
-describe('Book integation testing with Supertest, Mocha and Chai...', () => {
+describe('Book integation testing with Supertest, Mocha and Chai...', (done) => {
+  let server;
+
+  before((done) => {
+    server = app.listen(0, done);
+  });
+  after(() => {
+    server.close();
+  });
   it('get "/api/books" Should return an array', (done) => {
     request
       .get('/api/books')
@@ -10,6 +18,30 @@ describe('Book integation testing with Supertest, Mocha and Chai...', () => {
       .expect((response) => {
         expect(response.body).to.be.an('Array');
       })
+      .end(done);
+  });
+  it('post "/api/books" Should return a Book', (done) => {
+    request
+      .post('/api/books')
+      .set('Content-Type', 'application/json')
+      .send({
+          message: 'Hello, Server!'
+      })
+      .expect((response) => {
+        expect(response.body).to.deep.equal({});
+      })
+      .end(done);
+  });
+  it('post "/api/books" Should return an error "Title is required"', (done) => {
+    request
+      .post('/api/books')
+      .set('Content-Type', 'application/json')
+      .send({
+        author: 'test',
+        genre: 'test'
+      })
+      .expect(400)
+      .expect('Title is required')
       .end(done);
   });
 });
