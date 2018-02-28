996abe837bdcb8a1029d6e1e7ec1986b6ed91f66
Remi Kristelijn <rkristelijn@gmail.com>
2018-02-28 18:13:22 +0100

    fixed errors, cleaning up

--- api/books/book-adapter.js
+++ api/books/book-adapter.js
@@ -1,67 +1,95 @@
 let bookAdapter = (Book) => {
-  let _create = (book, done) => {
+  let _create = (book, bookAdapterErr, bookAdapterSuccess) => {
+    if (!book.title) {
+      bookAdapterErr('bookAdapter._save: ERROR: Title is required');
+      return;
+    }
     let _book = new Book(book);
     _book.save((err, book) => {
       if (err) {
-        console.log('bookAdapter._save: ERROR:', err);
-        done(err);
+        bookAdapterErr('bookAdapter._save: ERROR:' + err);
+        return;
       }
-      console.log('bookAdapter._save: SUCCESS:', book);
-      done(book);
+      //console.log('bookAdapter._save: SUCCESS:', book);
+      bookAdapterSuccess(book);
     });
   };
-  let _read = (book, done) => {
-    console.log('bookAdapter._read: reading book:', book);
+  let _read = (book, bookAdapterErr, bookAdapterSuccess) => {
+    //console.log('bookAdapter._read: reading book:', book);
+    let id = book.id;
+    if (!id) {
+      bookAdapterErr('bookAdapter._read: ERROR: book.id is required');
+      return;
+    }
+    if (!_isValidObjectId(id)) {
+      bookAdapterErr('bookAdapter._read: ERROR: Cast to ObjectId failed for value: ' + book.id);
+      return;
+    }
     Book.findById(book.id, (err, book) => {
       if (err) {
-        console.log('bookAdapter._read: ERROR:', err);
-        done(err);
+        bookAdapterErr('bookAdapter._read: ERROR:' + err);
+        return;
       }
-      console.log('bookAdapter._read: SUCCESS:', book);
-      done(book);
+      //console.log('bookAdapter._read: SUCCESS:', book);
+      if (!book) {
+        bookAdapterErr('bookAdapter._read: ERROR: book not found:' + id);
+        return;
+      }
+      bookAdapterSuccess(book);
     });
   };
-  let _update = (book, done) => {
-    console.log('bookAdapter._update: updating book:', book);
-    _read(book, (_book) => {
+  let _update = (book, bookAdapterErr, bookAdapterSuccess) => {
+    if (!_isValidObjectId(book.id)) {
+      bookAdapterErr('bookAdapter._update: ERROR: Cast to ObjectId failed for value: ' + book.id);
+      return;
+    }
+    //console.log('bookAdapter._update: updating book:', book);
+    _read(book, err => {
+      bookAdapterErr('bookAdapter._update: ERROR:' + err);
+      return;
+    }, (_book) => {
       for (let member in book) {
-        //if (!_book.hasOwnProperty(member) || typeof(_book[member]) === "function") continue;
-        console.log('bookAdapter._update: element in book:', member);
+        //console.log('bookAdapter._update: element in book:', member);
         if (member !== 'id') {
           _book[member] = book[member];
         }
       }
-
       _book.save((err, book) => {
         if (err) {
-          console.log('bookAdapter._update: ERROR:', err);
-          done(err);
+          bookAdapterErr('bookAdapter._update: ERROR:' + err);
+          return;
         }
-        console.log('bookAdapter._update: SUCCESS:', book);
-        done(book);
+        //console.log('bookAdapter._update: SUCCESS:', book);
+        bookAdapterSuccess(book);
       });
     });
   };
-  let _delete = (book, done) => {
-    Book.remove({_id: book.id}, (err) => {
+  let _delete = (book, bookAdapterErr, bookAdapterSuccess) => {
+    if (!_isValidObjectId(book.id)) {
+      bookAdapterErr('bookAdapter._delete: ERROR: Cast to ObjectId failed for value: ' + book.id);
+      return;
+    }
+    Book.remove({ _id: book.id }, (err) => {
       if (err) {
-        console.log('bookAdapter._delete: ERROR:', err);
-        done(err);
+        bookAdapterErr('bookAdapter._delete: ERROR:' + err);
+        return;
       }
-      console.log('bookAdapter._delete: SUCCESS:', book);
-      done(book);
+      //console.log('bookAdapter._delete: SUCCESS:', book);
+      bookAdapterSuccess(book);
     });
   };
-  let _query = (query, done) => {
-    console.log('bookAdapter._query: finding books:', query);
+  let _query = (query, bookAdapterErr, bookAdapterSuccess) => {
+    //console.log('bookAdapter._query: finding books:', query);
     Book.find(query, (err, books) => {
       if (err) {
-        console.log('bookAdapter._query: ERROR: ', err);
-        done(err);
+        bookAdapterErr('bookAdapter._query: ERROR: ' + err);
+        return;
       }
-      //console.log('bookAdapter: SUCCESS: ', books);
-      done(books);
+      bookAdapterSuccess(books);
     });
+  };
+  let _isValidObjectId = (id) => {
+    return (id.match(/^[0-9a-fA-F]{24}$/));
   };
   return {
     create: _create,
--- api/books/book-controller.js
+++ api/books/book-controller.js
@@ -1,31 +1,30 @@
-const Book = require('./book-model');
-let bookAdapter = require('./book-adapter')(Book);
-
 let bookController = (Book) => {
+  let bookAdapter = require('./book-adapter')(Book);
   let _create = (req, res) => {
-    //let book = new Book(req.body);
-    if (!req.body.title) {
-      res.status(400);
-      res.send('Title is required');
-    } else {
-      bookAdapter.create(req.body, (book) => {
-        res.status(201);
-        res.json(book);
-      });
-      //book.save();
-      // let returnBook = Object.assign({}, book._doc);
-      // returnBook.links = {};
-      // returnBook.links.filterByThisGenre = `${req.protocol}://${req.headers.host}`
-      //   + '/api/books?genre='
-      //   + book.genre.replace(' ', '%20');
-      //res.status(201);
-      // res.json(returnBook);
-      //res.json(book);
-    }
+
+    bookAdapter.create(req.body, err => {
+      res.status(400).send(err);
+    }, (book) => {
+
+      res.status(201);
+      res.json(book);
+
+    });
+    //book.save();
+    // let returnBook = Object.assign({}, book._doc);
+    // returnBook.links = {};
+    // returnBook.links.filterByThisGenre = `${req.protocol}://${req.headers.host}`
+    //   + '/api/books?genre='
+    //   + book.genre.replace(' ', '%20');
+    //res.status(201);
+    // res.json(returnBook);
+    //res.json(book);
+
   };
   let _readAll = (req, res) => {
-    bookAdapter.query({}, (books) => {
-      //console.log("_readAll:", books);
+    bookAdapter.query({}, (err) => {
+      res.status(400).send(err);
+    }, (books) => {
       res.json(books);
     });
     // let query = {};
@@ -53,12 +52,22 @@ let bookController = (Book) => {
     if (!req.params.bookId) {
       res.status(400);
       res.send('bookId is required');
-    } else if (!_isObjectId(req.params.bookId)) {
-      res.status(400);
-      res.send(`Cast to ObjectId failed for value ${req.params.bookId}`);
+      // } else if (!_isObjectId(req.params.bookId)) {
+      //   res.status(400);
+      //   res.send(`Cast to ObjectId failed for value ${req.params.bookId}`);
+      //
     } else {
-      bookAdapter.read({ id: req.params.bookId }, (book) => {
-        res.json(book);
+      bookAdapter.read({
+        id: req.params.bookId
+      }, err => {
+        res.status(400).send(err);
+      }, book => {
+        if (!book) {
+          console.log('bookController._readOne: ERROR: No book found')
+          res.status(404).send('No book found');
+        } else {
+          res.json(book);
+        }
       });
       // Book.findById(req.params.bookId, (err, book) => {
       //   if (err) {
@@ -79,49 +88,50 @@ let bookController = (Book) => {
     }
   };
   let _updateOne = (req, res) => {
-    if (!req.params.bookId) {
-      res.status(400);
-      res.send('bookId is required');
-    } else if (!_isObjectId(req.params.bookId)) {
-      res.status(400);
-      res.send(`Cast to ObjectId failed for value ${req.params.bookId}`);
-    } else {
-      req.body.id = req.params.bookId;
-      bookAdapter.update(req.body, (book) => {
+
+    req.body.id = req.params.bookId;
+    bookAdapter.update(req.body,
+      err => {
+        res.status(400).send(err);
+      }, (book) => {
         res.json(book);
       });
-      // Book.findById(req.params.bookId, (err, book) => {
-      //   if (err) {
-      //     res.status(500).send(err);
-      //   } else if (book) {
-      //     console.log('found book:', book);
-      //     if (req.body.title) {
-      //       req.book.title = req.body.title;
-      //     }
-      //     if (req.body.author) {
-      //       req.book.author = req.body.author;
-      //     }
-      //     if (req.body.genre) {
-      //       req.book.genre = req.body.genre;
-      //     }
-      //     if (req.body.read) {
-      //       req.book.read = req.body.read;
-      //     }
-      //     console.log('saving...', req.book);
-      //     req.book.save((err) => {
-      //       if (err) {
-      //         res.status(500).send(err);
-      //       } else {
-      //         res.json(req.book);
-      //       }
-      //     });
-      //   }
-      // });
-    }
+    // Book.findById(req.params.bookId, (err, book) => {
+    //   if (err) {
+    //     res.status(500).send(err);
+    //   } else if (book) {
+    //     console.log('found book:', book);
+    //     if (req.body.title) {
+    //       req.book.title = req.body.title;
+    //     }
+    //     if (req.body.author) {
+    //       req.book.author = req.body.author;
+    //     }
+    //     if (req.body.genre) {
+    //       req.book.genre = req.body.genre;
+    //     }
+    //     if (req.body.read) {
+    //       req.book.read = req.body.read;
+    //     }
+    //     console.log('saving...', req.book);
+    //     req.book.save((err) => {
+    //       if (err) {
+    //         res.status(500).send(err);
+    //       } else {
+    //         res.json(req.book);
+    //       }
+    //     });
+    //   }
+    // });
+
   };
 
   let _deleteOne = (req, res) => {
-    bookAdapter.delete({ id: req.params.bookId }, (book) => {
+    bookAdapter.delete({
+      id: req.params.bookId
+    }, err => {
+      res.status(400).send(err);
+    }, book => {
       res.status(204).send(`Removed ${book}`);
     });
   };
@@ -149,10 +159,6 @@ let bookController = (Book) => {
   //     }
   //   });
   // };
-
-  let _isObjectId = (id) => {
-    return (id.match(/^[0-9a-fA-F]{24}$/));
-  };
 
   return {
     create: _create,
