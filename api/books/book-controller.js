eed57d31148ce661e4466be2f0f9860d51b73f8b
Remi Kristelijn <rkristelijn@gmail.com>
2018-03-01 09:03:15 +0100

    save for Sai

--- api/books/book-controller.js
+++ api/books/book-controller.js
@@ -1,8 +1,16 @@
-let bookController = (Book) => {
-  let bookAdapter = require('./book-adapter')(Book);
+let bookController = (bookAdapter) => {
+  let _hasBookId = (req, res) => {
+    if (!req.params || !req.params.bookId) {
+      res.status(400);
+      res.send('bookId is required');
+      return false;
+    }
+    return true;
+  };
   let _create = (req, res) => {
     bookAdapter.create(req.body, err => {
-      res.status(400).send(err);
+      res.status(400);
+      res.send(err);
     }, (book) => {
       res.status(201);
       res.json(book);
@@ -10,41 +18,50 @@ let bookController = (Book) => {
   };
   let _readAll = (req, res) => {
     bookAdapter.query({}, (err) => {
-      res.status(400).send(err);
+      res.status(400);
+      res.send(err);
     }, (books) => {
       res.json(books);
     });
   };
   let _readOne = (req, res) => {
-    if (!req.params.bookId) {
-      res.status(400);
-      res.send('bookId is required');
+    if (!_hasBookId(req, res)) {
       return;
     }
     bookAdapter.read({
       id: req.params.bookId
     }, err => {
-      res.status(400).send(err);
+      res.status(400);
+      res.end(err);
     }, book => {
       res.json(book);
     });
   };
   let _updateOne = (req, res) => {
+    if (!_hasBookId(req, res)) {
+      return;
+    }
     req.body.id = req.params.bookId;
     bookAdapter.update(req.body,
       err => {
-        res.status(400).send(err);
+        res.status(400);
+        res.send(err);
       }, (book) => {
         res.json(book);
       });
   };
   let _deleteOne = (req, res) => {
+    if (!_hasBookId(req, res)) {
+      return;
+    }
     bookAdapter.delete({
       id: req.params.bookId
     }, err => {
-      res.status(400).send(err);
+      res.status(400);
+      res.send(err);
     }, book => {
-      res.status(204).send(`Removed ${book}`);
+      res.status(204);
+      res.send(`Removed ${book}`);
     });
   };
 
--- api/books/book-controller.unit.test.js
+++ api/books/book-controller.unit.test.js
@@ -1,9 +1,109 @@
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
+let bookAdapterMock;
+let bookController;
+let req;
+let res;
+
+beforeEach((done) => {
+  bookAdapterMock = {
+    create: sinon.spy(),
+    query: sinon.spy(),
+    read: sinon.spy(),
+    update: sinon.spy(),
+    delete: sinon.spy()
+  };
+  bookController = require('./book-controller')(bookAdapterMock);
+  req = {
+    body: 'bodyid',
+    params: {
+      bookId: 'paramsid'
+    }
+  };
+  res = {
+    status: sinon.spy(),
+    json: sinon.spy(),
+    send: sinon.spy()
+  };
+  done();
+});
 describe('book-controller', () => {
-  describe('some part', () => {
-    it('Should test', (done) => {
-      expect(true).to.equal(true);
+  describe('Create', () => {
+    it('Should create', (done) => {
+      bookController.create(req, res);
+      expect(bookAdapterMock.create.args[0][0]).to.be.equal('bodyid');
+      done();
+    });
+  });
+  describe('ReadAll', () => {
+    it('Should query', (done) => {
+      bookController.readAll(req, res);
+      expect(bookAdapterMock.query.args[0][0]).to.be.deep.equal({});
+      done();
+    });
+  });
+  describe('ReadOne', () => {
+    it('Should read', (done) => {
+      bookController.readOne(req, res);
+      expect(bookAdapterMock.read.args[0][0]).to.be.deep.equal({ id: 'paramsid' });
+      done();
+    });
+    it('Should throw error on empty req.params', (done) => {
+      req.params = {};
+      bookController.readOne(req, res);
+      expect(res.status.args[0][0]).to.be.equal(400);
+      expect(res.send.args[0][0]).to.be.equal('bookId is required');
+      done();
+    });
+    it('Should throw error on empty req', (done) => {
+      req = {};
+      bookController.readOne(req, res);
+      expect(res.status.args[0][0]).to.be.equal(400);
+      expect(res.send.args[0][0]).to.be.equal('bookId is required');
+      done();
+    });
+  });
+  describe('UpdateOne', () => {
+    it('Should update', (done) => {
+      req.body = { id: 'object' };
+      bookController.updateOne(req, res);
+      expect(bookAdapterMock.update.args[0][0]).to.be.deep.equal({ id: 'paramsid' });
+      done();
+    });
+    it('Should throw error on empty req.params', (done) => {
+      req.params = {};
+      bookController.updateOne(req, res);
+      expect(res.status.args[0][0]).to.be.equal(400);
+      expect(res.send.args[0][0]).to.be.equal('bookId is required');
+      done();
+    });
+    it('Should throw error on empty req', (done) => {
+      req = {};
+      bookController.updateOne(req, res);
+      expect(res.status.args[0][0]).to.be.equal(400);
+      expect(res.send.args[0][0]).to.be.equal('bookId is required');
+      done();
+    });
+  });
+  describe('DeleteOne', () => {
+    it('Should delete', (done) => {
+      bookController.deleteOne(req, res);
+      expect(bookAdapterMock.delete.args[0][0]).to.be.deep.equal({ id: 'paramsid' });
+      done();
+    });
+    it('Should throw error on empty req.params', (done) => {
+      req.params = {};
+      bookController.deleteOne(req, res);
+      expect(res.status.args[0][0]).to.be.equal(400);
+      expect(res.send.args[0][0]).to.be.equal('bookId is required');
+      done();
+    });
+    it('Should throw error on empty req', (done) => {
+      req = {};
+      bookController.deleteOne(req, res);
+      expect(res.status.args[0][0]).to.be.equal(400);
+      expect(res.send.args[0][0]).to.be.equal('bookId is required');
       done();
     });
   });
