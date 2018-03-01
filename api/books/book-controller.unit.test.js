4aca2948f7efb71a24716673c07c307213801fbe
Remi Kristelijn <rkristelijn@gmail.com>
2018-03-01 06:32:15 +0100

    added separate unit test for controller

---
+++ api/books/book-controller.unit.test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('book-controller', () => {
+  describe('some part', () => {
+    it('Should test', (done) => {
+      expect(true).to.equal(true);
+      done();
+    });
+  });
+});
