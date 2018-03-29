255bf3a79f9984494544b5a0fdcbbd005ddc2341
Remi Kristelijn <rkristelijn@gmail.com>
2018-03-29 12:46:56 +0200

    working login form with pug

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-shadow */
 const express = require('express');
 const passport = require('passport');
+const pug = require('pug');
 
 let routes = () => {
   let authRouter = express.Router();
@@ -13,21 +14,31 @@ let routes = () => {
 
   authRouter.route('/local/login')
     .get((req, res) => {
-      console.log('in get');
-      res.end('login form');
+      let loggedIn = false;
+      if (req.user) {
+        loggedIn = true;
+      }
+      res.end(pug.renderFile(__dirname + '/login-form.pug',
+        {
+          actionUrl: '/api/auth/local/login',
+          loggedIn: loggedIn
+        }
+      ));
     })
     .post(
       passport.authenticate('local'),
       (req, res) => {
         console.log('in post of login');
-        res.end('login success');
+        //res.end('login success');
+        res.redirect('/api/auth/local/login');
       });
 
   authRouter.route('/local/logout')
     .get((req, res) => {
       console.log('in get of logout');
       req.logout();
-      res.end('logout success');
+      //res.end('logout success');
+      res.redirect('/api/auth/local/login');
     });
 
   return authRouter;
